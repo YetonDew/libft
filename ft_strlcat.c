1dcca23dbcc4e179fc1e0ec73bc137112ded0382
YetonDew <ariasjefferson@hotmail.com>
2025-09-30 18:26:19 +0200

    strlcat

---
+++ ft_strlcat.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ajeffers <ajeffers@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/30 17:24:22 by ajeffers          #+#    #+#             */
+/*   Updated: 2025/09/30 18:24:08 by ajeffers         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+size_t	ft_strlcat(char *dst, const char *src, size_t size)
+{
+	size_t	src_len;
+	size_t	dst_len;
+	size_t	free_space;
+	size_t	i;
+
+	src_len = ft_strlen(src);
+	dst_len = ft_strlen(dst);
+	if (dst_len >= size)
+		return (size + src_len);
+	free_space = size - dst_len - 1;
+	i = 0;
+	while (dst[i] && free_space < size)
+	{
+		dst[dst_len + i] = src[i];
+		i++;
+	}
+	dst[dst_len + i] = '\0';
+	return (src_len + dst_len);
+}
